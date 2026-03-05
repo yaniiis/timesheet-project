23ec982a32bddcb1bc88937d13b17443038ddc1e
Yanis <yanisbouchaib28@gmail.com>
2026-03-05 14:26:15 +0100

    correction U

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.services.IUserService;
 
-// userRestControl
 @RestController // = @Controller + @ResponseBody 
 @RequestMapping("/user")
 public class UserRestControl {
